18c9c9a31b260e336a0cc7741ce0d00efe6c1c2f
Sofia <SCastro@dal.ca>
2020-10-10 01:32:50 -0500

    Issue #14 Check credentials against database to see if they match any. New branch because I couldn't get the login test branch to accept firebase but when that is figured out then will merge. Has errors because User object and database are not on at the time of my commit I think.

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myfirstapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DataSnapshot;
+
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -10,4 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public boolean credentialsCheck(String firstname, String lastname, String email, String password, @NonNull DataSnapshot dataSnapshot){
+        Iterator<DataSnapshot> it = dataSnapshot.getChildren().iterator();
+        while (it.hasNext()) {
+            User kv = it.next().getValue(User.class);
+            if(firstname.equals(kv.getFirstName())&&lastname.equals(kv.getLastName())&&email.equals(kv.getEmail())&&password.equals(kv.getPassword())){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
