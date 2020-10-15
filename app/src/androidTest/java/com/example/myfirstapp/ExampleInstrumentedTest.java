8f86ef46affd300f335f868f13e1454c2658538a
Emerson Collins <em931418@dal.ca>
2020-10-15 14:01:59 -0300

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
@@ -1,10 +1,9 @@
 package com.example.myfirstapp;
 
 import android.content.Context;
-//import androidx.test.runner.AndroidJUnit4;
-//import androidx.test.InstrumentationRegistry;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
+import androidx.test.InstrumentationRegistry;
+
 
 
 import org.junit.Test;
