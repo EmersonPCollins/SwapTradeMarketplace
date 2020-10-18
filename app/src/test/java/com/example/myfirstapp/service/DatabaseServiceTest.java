44426a66f1e463f71c843d8466a413e8d69353d5
ecollins <em931418@dal.ca>
2020-10-18 19:30:25 -0300

    With U.4 V2 and Database added

--- app/src/test/java/com/example/myfirstapp/service/DatabaseServiceTest.java
+++ app/src/test/java/com/example/myfirstapp/service/DatabaseServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import static org.mockito.Mockito.*;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
