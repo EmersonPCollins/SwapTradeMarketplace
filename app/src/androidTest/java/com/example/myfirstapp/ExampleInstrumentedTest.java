6a63efd63623b4e6b623c63c017de121cc46bbf9
Sofia <SCastro@dal.ca>
2020-10-19 19:48:33 -0500

    Fixed some errors with layout constraint, added the test code in (doesn't work) and merged master into the branch

--- app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
@@ -1,28 +1,92 @@
 package com.example.myfirstapp;
+import org.junit.Rule;
 
 import android.content.Context;
-import androidx.test.runner.AndroidJUnit4;
-import androidx.test.InstrumentationRegistry;
 
-
+import androidx.annotation.NonNull;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.myfirstapp", appContext.getPackageName());
+    @Rule
+    public ActivityTestRule<GoodsActivity> activityActivityTestRule = new ActivityTestRule<GoodsActivity>(GoodsActivity.class);
+
+    @Test
+    public void testEmptyTitle() {
+        onView(withId(R.id.titleText))
+                .perform(clearText(),typeText(""));
+        onView(withId(R.id.submitGoodButton))
+                .perform(click());
+        onView(withId(R.id.errorMessageTextView))
+                .check(matches(withText("Error: Enter a valid title.")));
+    }
+    @Test
+    public void testEmptyDescription() {
+        onView(withId(R.id.descriptionText))
+                .perform(click())
+                .perform(typeText(""));
+        onView(withId(R.id.submitGoodButton))
+                .perform(click());
+        onView(withId(R.id.errorMessageTextView))
+                .check(matches(withText("Error: Enter a description.")));
+    }
+
+    @Test
+    public void badTestDate() {
+        onView(withId(R.id.dateText))
+                .perform(click())
+                .perform(typeText(""));
+        onView(withId(R.id.submitGoodButton))
+                .perform(click());
+        onView(withId(R.id.errorMessageTextView))
+                .check(matches(withText("Error: Enter a valid date.")));
+        onView(withId(R.id.dateText))
+                .perform(click())
+                .perform(typeText("1/1/2020"));
+        onView(withId(R.id.submitGoodButton))
+                .perform(click());
+        onView(withId(R.id.errorMessageTextView))
+                .check(matches(withText("Error: Enter a valid date.")));
+    }
+    @Test
+    public void testEmptyLocation() {
+        onView(withId(R.id.locationText))
+                .perform(click())
+                .perform(typeText(""));
+        onView(withId(R.id.submitGoodButton))
+                .perform(click());
+        onView(withId(R.id.errorMessageTextView))
+                .check(matches(withText("Error: Enter a location.")));
+    }
+    @Test
+    public void testValidDetails() {
+        onView(withId(R.id.titleText))
+                .perform(click())
+                .perform(typeText("My Title"));
+        onView(withId(R.id.descriptionText))
+                .perform(click())
+                .perform(typeText("My Description"));
+        onView(withId(R.id.dateText))
+                .perform(click())
+                .perform(typeText("01/11/2020"));
+        onView(withId(R.id.locationText))
+                .perform(click())
+                .perform(typeText("123 My Street Halifax NS"));
+        onView(withId(R.id.submitGoodButton))
+                .perform(click());
+        onView(withId(R.id.errorMessageTextView))
+                .check(matches(withText("")));
     }
 }
