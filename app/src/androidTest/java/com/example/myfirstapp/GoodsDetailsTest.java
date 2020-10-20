3145ec2de179496047e751f78210011dfcf44d34
Mellat Mafei <ml613428@dal.ca>
2020-10-20 19:15:28 -0400

    Bug fix: Tests weren't running due to soft keyboard not closing

--- app/src/androidTest/java/com/example/myfirstapp/GoodsDetailsTest.java
+++ app/src/androidTest/java/com/example/myfirstapp/GoodsDetailsTest.java
@@ -3,10 +3,12 @@ import android.app.Activity;
 
 import org.junit.Rule;
 import org.junit.Test;
-import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
+
+import androidx.test.espresso.ViewAction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 public class GoodsDetailsTest {
@@ -19,7 +21,7 @@ public class GoodsDetailsTest {
     public void testEmptyTitle() {
         onView(withId(R.id.titleText))
                 .perform(click())
-                .perform(typeText(""));
+                .perform(typeText(""), closeSoftKeyboard());
         onView(withId(R.id.submitGoodButton))
                 .perform(click());
         onView(withId(R.id.errorMessageTextView))
@@ -29,7 +31,7 @@ public class GoodsDetailsTest {
     public void testEmptyDescription() {
         onView(withId(R.id.descriptionText))
                 .perform(click())
-                .perform(typeText(""));
+                .perform(typeText(""), closeSoftKeyboard());
         onView(withId(R.id.submitGoodButton))
                 .perform(click());
         onView(withId(R.id.errorMessageTextView))
@@ -40,14 +42,14 @@ public class GoodsDetailsTest {
     public void badTestDate() {
         onView(withId(R.id.dateText))
                 .perform(click())
-                .perform(typeText(""));
+                .perform(typeText(""), closeSoftKeyboard());
         onView(withId(R.id.submitGoodButton))
                 .perform(click());
         onView(withId(R.id.errorMessageTextView))
                 .check(matches(withText("Error: Enter a valid date.")));
         onView(withId(R.id.dateText))
                 .perform(click())
-                .perform(typeText("1/1/2020"));
+                .perform(typeText("1/1/2020"), closeSoftKeyboard());
         onView(withId(R.id.submitGoodButton))
                 .perform(click());
         onView(withId(R.id.errorMessageTextView))
@@ -57,7 +59,7 @@ public class GoodsDetailsTest {
     public void testEmptyLocation() {
         onView(withId(R.id.locationText))
                 .perform(click())
-                .perform(typeText(""));
+                .perform(typeText(""), closeSoftKeyboard());
         onView(withId(R.id.submitGoodButton))
                 .perform(click());
         onView(withId(R.id.errorMessageTextView))
@@ -67,16 +69,16 @@ public class GoodsDetailsTest {
     public void testValidDetails() {
         onView(withId(R.id.titleText))
                 .perform(click())
-                .perform(typeText("My Title"));
-        onView(withId(R.id.descriptionText))
-                .perform(click())
-                .perform(typeText("My Description"));
-        onView(withId(R.id.dateText))
-                .perform(click())
-                .perform(typeText("01/11/2020"));
+                .perform(typeText("My Title"), closeSoftKeyboard());
         onView(withId(R.id.locationText))
                 .perform(click())
-                .perform(typeText("123 My Street Halifax NS"));
+                .perform(typeText("123 My Street Halifax NS"), closeSoftKeyboard());
+        onView(withId(R.id.dateText))
+                .perform(click())
+                .perform(typeText("01/11/2020"), closeSoftKeyboard());
+        onView(withId(R.id.descriptionText))
+                .perform(click(), closeSoftKeyboard())
+                .perform(typeText("My Description"), closeSoftKeyboard());
         onView(withId(R.id.submitGoodButton))
                 .perform(click());
         onView(withId(R.id.errorMessageTextView))
