155bad266a18115ebe6a6785ae23a742110cb0d5
Mellat Mafei <ml613428@dal.ca>
2020-10-20 20:58:36 -0400

    Aded Sofia's validation work that got removed back into the comment

--- app/src/main/java/com/example/myfirstapp/GoodsActivity.java
+++ app/src/main/java/com/example/myfirstapp/GoodsActivity.java
@@ -131,41 +131,66 @@ public class GoodsActivity extends AppCompatActivity implements AdapterView.OnIt
 
 
 
-
     /**
      *
-     * To be examined
+     * to be examined
      *
-     @RequiresApi(api = Build.VERSION_CODES.O)
-     public void validate() throws ParseException {
-     EditText title = (EditText) findViewById(R.id.titleText);
-     EditText description = (EditText) findViewById(R.id.descriptionText);
-     EditText date = (EditText) findViewById(R.id.dateText);
-     EditText location = (EditText) findViewById(R.id.locationText);
-     TextView errorMessage = (TextView) findViewById(R.id.errorMessageTextView);
-     // if(!title.getText().toString().isEmpty() && !description.getText().toString().isEmpty() && !location.getText().toString().isEmpty() && goodDate(date.getText().toString())){
-     //ADD TO DATABASE
-     //replace "email@email.com" with the logged in user email
-
-     }
-
-
-     //METHODS I USE TO CHECK THE DATE ARE VALID REQUIRE THIS API PART
-     @RequiresApi(api = Build.VERSION_CODES.O)
-     public boolean goodDate(String sdate) throws ParseException {
-     if(sdate.equals("")){
-     return false;
-     }
-     LocalDate d1 = java.time.LocalDate.now();
-     DateTimeFormatter f = DateTimeFormatter.ofPattern( "dd-MM-yyyy" );
-     LocalDate date = LocalDate.parse(sdate,f);
-     if(!sdate.matches("^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$")){
-     return false;
-     }
-     if(date.isBefore(d1)){
-     return false;
-     }
-     return true;
-     }
-     **/
+     *
+     *     @RequiresApi(api = Build.VERSION_CODES.O)
+     *     public void validate() throws ParseException {
+     *         EditText title = (EditText) findViewById(R.id.titleText);
+     *         EditText description = (EditText) findViewById(R.id.descriptionText);
+     *         EditText date = (EditText) findViewById(R.id.dateText);
+     *         EditText location = (EditText) findViewById(R.id.locationText);
+     *         TextView errorMessage = (TextView) findViewById(R.id.errorMessageTextView);
+     *         if(!title.getText().toString().isEmpty() && !description.getText().toString().isEmpty() && !location.getText().toString().isEmpty() && goodDate(date.getText().toString())){
+     *             //ADD TO DATABASE
+     *             //replace "email@email.com" with the logged in user email
+     *             insertGood(title.getText().toString(), date.getText().toString(), "email@email.com");
+     *
+     *         }else{
+     *             if(title.getText().toString().isEmpty()){
+     *                 errorMessage.setText("Error: Enter a valid title.");
+     *             }else{
+     *                 if(description.getText().toString().isEmpty()){
+     *                     errorMessage.setText("Error: Enter a description.");
+     *                 }else{
+     *                     if(!goodDate(date.getText().toString())){
+     *                         errorMessage.setText("Error: Enter a valid date.");
+     *                     }else{
+     *                         if(location.getText().toString().equals("")){
+     *                             errorMessage.setText("Error: Enter a location.");
+     *                         }
+     *                     }
+     *                 }
+     *             }
+     *         }
+     *     }
+     *
+     *     //populate after merging the goods db and pass to it the firebase db used in the sign in
+     *     public void insertGood(String title, String date, String user_email){
+     *         Good good = new Good(title, date, user_email);
+     *         //get the db connection
+     *         DatabaseService db = new DatabaseService();
+     *         db.writeGood(good);
+     *     }
+     *
+     *     //METHODS I USE TO CHECK THE DATE ARE VALID REQUIRE THIS API PART
+     *     @RequiresApi(api = Build.VERSION_CODES.O)
+     *     public boolean goodDate(String sdate) throws ParseException {
+     *         if(sdate.equals("")){
+     *             return false;
+     *         }
+     *         LocalDate d1 = java.time.LocalDate.now();
+     *         DateTimeFormatter f = DateTimeFormatter.ofPattern( "dd-MM-yyyy" );
+     *         LocalDate date = LocalDate.parse(sdate,f);
+     *         if(!sdate.matches("^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$")){
+     *             return false;
+     *         }
+     *         if(date.isBefore(d1)){
+     *             return false;
+     *         }
+     *         return true;
+     *     }
+     * **/
 }
