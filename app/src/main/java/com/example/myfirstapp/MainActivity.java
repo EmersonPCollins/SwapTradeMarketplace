6de8130e041637ba2ec3e0f64d9127e071a3aaf1
ecollins <em931418@dal.ca>
2020-10-22 14:28:14 -0300

    Small error in MainActivity fixed

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_goods);
+        setContentView(R.layout.activity_main);
 
         databaseService = new DatabaseService(FirebaseDatabase.getInstance());
 
