6f32d1e9225e9039665f4d9597a9a8f7583d2093
JacobTLeBlanc <leblancthomasjacob@gmail.com>
2020-12-03 13:51:03 -0400

    Added basic search functionality

--- app/src/main/java/com/example/myfirstapp/Activity/SearchActivity.java
+++ app/src/main/java/com/example/myfirstapp/Activity/SearchActivity.java
@@ -1,25 +1,78 @@
 package com.example.myfirstapp.Activity;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.SearchView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-import androidx.annotation.RequiresApi;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myfirstapp.R;
+import com.example.myfirstapp.domain.Good;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-import java.text.ParseException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        final ArrayList<Good> listOfGoods = new ArrayList<Good>();
+
+        final Spinner categorySpinner = (Spinner) findViewById(R.id.categoriesSearchSpinner);
+        final Spinner locationSpinner = (Spinner) findViewById(R.id.locationSearchSpinner);
+        SearchView searchView = (SearchView) findViewById(R.id.searchField);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(final String query) {
+
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("goods");
+                final String categorySpinnerValue = categorySpinner.getSelectedItem().toString();
+                final String locationSpinnerValue = locationSpinner.getSelectedItem().toString();
+
+                ref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot adSnapshot : dataSnapshot.getChildren()) {
+                            Good good = adSnapshot.getValue(Good.class);
+                            if (good == null || good.getTitle() == null || good.getType() == null || good.getExchange_location() == null) {
+                                continue;
+                            }
+
+                            if (good.getTitle().equals(query) && good.getType().equals(categorySpinnerValue) && good.getExchange_location().contains(locationSpinnerValue)) {
+                                listOfGoods.add(good);
+
+                                TextView goodName = (TextView) findViewById(R.id.goodName);
+                                TextView goodCategory = (TextView) findViewById(R.id.goodCategory);
+                                TextView goodLocation = (TextView) findViewById(R.id.goodLocation);
+
+                                goodName.setText(good.getTitle());
+                                goodCategory.setText(good.getType());
+                                goodLocation.setText(good.getExchange_location());
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }});
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
+
+
 }
